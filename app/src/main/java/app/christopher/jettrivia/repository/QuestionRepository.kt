25b79fcd70fde7bce9e67d7ef0a3eb79b262c1d3
Krystofr <krystofrnanju@gmail.com>
2022-07-13 16:19:13 +0100

    Finishing up the Repository class to use the Wrapper class.

--- app/src/main/java/app/christopher/jettrivia/repository/QuestionRepository.kt
+++ app/src/main/java/app/christopher/jettrivia/repository/QuestionRepository.kt
@@ -1,5 +1,6 @@
 package app.christopher.jettrivia.repository
 
+import android.util.Log
 import app.christopher.jettrivia.data.DataOrException
 import app.christopher.jettrivia.model.QuestionItem
 import app.christopher.jettrivia.network.QuestionApi
@@ -7,6 +8,19 @@ import javax.inject.Inject
 
 class QuestionRepository @Inject constructor(private val api: QuestionApi) {
 
-    private val listOfQuestions =
+    private val dataOrException =
         DataOrException<ArrayList<QuestionItem>, Boolean, Exception>()
+
+    suspend fun getAllQuestions() : DataOrException<ArrayList<QuestionItem>, Boolean, Exception>{
+        try {
+            dataOrException.loading = true
+            dataOrException.data = api.getAllQuestions()
+            if (dataOrException.data.toString().isNotEmpty()) dataOrException.loading = false
+
+        } catch (ex: Exception) {
+            dataOrException.e = ex
+            Log.d("Exception", "getAllQuestions(): ${dataOrException.e!!.localizedMessage?.toString()}")
+        }
+        return dataOrException
+    }
 }
